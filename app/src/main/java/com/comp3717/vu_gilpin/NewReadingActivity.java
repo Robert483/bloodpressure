29002289eec29a3817f8a6f84449471b77623aad
Robert <pvu9@my.bcit.ca>
2020-03-02 12:23:46 -0800

    Finished basic layout for NewReadingActivity.

---
+++ app/src/main/java/com/comp3717/vu_gilpin/NewReadingActivity.java
@@ -0,0 +1,14 @@
+package com.comp3717.vu_gilpin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewReadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_reading);
+    }
+}
