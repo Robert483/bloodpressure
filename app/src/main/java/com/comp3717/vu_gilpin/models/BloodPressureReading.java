3d253133691baeb490b7ff2abf6df63ee087d445
Robert <pvu9@my.bcit.ca>
2020-03-02 13:13:05 -0800

    Finished basic layout for MainActivity.

---
+++ app/src/main/java/com/comp3717/vu_gilpin/models/BloodPressureReading.java
@@ -0,0 +1,10 @@
+package com.comp3717.vu_gilpin.models;
+
+import java.util.Date;
+
+public class BloodPressureReading {
+    private String userId;
+    private Date readingDate;
+    private Integer systolicReading;
+    private Integer diastolicReading;
+}
